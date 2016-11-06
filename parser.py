adbfe8c83b8ed710b460d62c7d33b8576bfd6148
scotchfield <swrittenb@gmail.com>
2016-11-06 20:35:34 +0000

    Adds working parser with replacements and variables
    This feels kind of cool! You can define set characters or adjectives
    or anything in the story liners, and if they exist, they'll be used.
    If they don't exist, they'll be generated, and will propagate through
    the rest of the story as it's created.
    
    Next is importing from a file instead of reading from a predefined
    string, and expanding the rules. Also, improve the error handling.

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import random
 import shlex
 import sys
 
@@ -11,9 +12,9 @@ def dead( line, st ):
     sys.exit(1)
 
 def parse( data ):
-    story_variables = {}
+    story_lists = {}
     story_rules = {}
-    story_generate = False
+    story_generate = []
 
     data = data.split( '\n' )
     in_rule = False
@@ -23,30 +24,81 @@ def parse( data ):
         line_num += 1
 
         line = line.strip()
-        line_tokens = shlex.split( line )
+        line_tokens = shlex.split( line, posix=False )
 
         if len( line_tokens ) == 0:
             in_rule = False
         elif not in_rule:
             if line_tokens[0].lower() == 'use':
                 try:
-                    story_variables[line_tokens[1]] = getFromFile( line_tokens[2] )
+                    story_lists[line_tokens[1]] = getFromFile( line_tokens[2] )
                 except IOError:
                     dead( line_num, 'Could not open ' + line_tokens[2] )
             elif line_tokens[0].lower() == 'generate':
-                story_generate = line_tokens[1]
-                print 'oh yeah ' + story_generate
+                story_generate = line_tokens[1:]
             else:
                 in_rule = line_tokens[0]
                 story_rules[in_rule] = []
         else:
             story_rules[in_rule].append( line_tokens )
 
-    return ( story_variables, story_rules, story_generate )
+    return ( story_generate, story_lists, story_rules )
 
-st = '''Use Names "text/firstnames.txt"
-Use Adjectives "text/adjectives.txt"
-Use Objects "text/objects.txt"
+def unusedToken( token ):
+    if len( token ) > 0 and token[0] != '"':
+        return True
+
+    return False
+
+def unusedCount( rule ):
+    count = 0
+
+    for token in rule:
+        if unusedToken( token ):
+            count += 1
+
+    return count
+
+def isVariable( token ):
+    return token.find( '.' ) > -1
+
+def getVariable( token, variables, lists ):
+    token_obj = token.split( '.' )
+
+    var_list = token_obj[0]
+    var_id = token_obj[1]
+
+    if var_id not in variables:
+        variables[var_id] = random.choice( lists[var_list] )
+
+    return variables[var_id]
+
+def generate( source, lists, rules, variables ):
+    print( 'Starting rule: ' + str( source ) )
+
+    while unusedCount( source ) > 0:
+        result = []
+
+        for token in source:
+            if not unusedToken( token ):
+                result.append( token )
+            elif isVariable( token ):
+                result.append( '"' + getVariable( token, variables, lists ) + '"' )
+            else:
+                result.extend( random.choice( rules[token] ) )
+
+        source = result
+
+    source = [ s.replace( '"', '' ) for s in source ]
+    source = filter( None, source )
+
+    return ''.join( source )
+
+
+st = '''
+Use Names text/firstnames.txt
+Use Adjectives text/adjectives.txt
+Use Objects text/objects.txt
 
 Story
   Introduction Middle End
@@ -55,22 +107,22 @@ Introduction
   AtFirst Describe
 
 AtFirst
-  "Once upon a time, there was a hero named " Names.Protagonist
+  "Once upon a time, there was a hero named " Names.Protagonist ". "
 
 Describe
-  Names.Protagonist " was a " Adjectives.ProtagonistAdjective " fighter."
-  Names.Protagonist "was a " Adjectives.ProtagonistAdjective " warrior."
+  Names.Protagonist " was a " Adjectives.ProtagonistAdjective " fighter. "
+  Names.Protagonist " was a " Adjectives.ProtagonistAdjective " warrior. "
 
 Middle
-  "Then " Names.Protagonist " went for a " Adjectives.ProtagonistAdjective " walk."
+  "Then " Names.Protagonist " went for a " Adjectives.ProtagonistAdjective " walk. "
 
 End
-  "And " Names.Protagonist " died."
+  "And " Names.Protagonist " died. "
 
 Generate Story'''
 
-( story_variables, story_rules, story_generate ) = parse( st )
 
-#print story_variables
-#print story_rules
-print story_generate
+( story_generate, story_lists, story_rules ) = parse( st )
+result = generate( story_generate, story_lists, story_rules, {} )
+
+print( result )
